69226828eba2fde65db02320cce210c337983962
Minh Nguyen <50205705+ABCbum@users.noreply.github.com>
2025-12-24 17:37:51 +0800

    more ctrip cases + more robust browser start

--- browsers/browser.sh
+++ browsers/browser.sh
@@ -42,22 +42,23 @@ fi
 mkdir -p "$PROFILE_DIR"
 
 # Build Chrome arguments
-CHROME_ARGS="
-    --remote-debugging-port=${PORT}
-    --remote-debugging-address=0.0.0.0
-    --user-data-dir=${PROFILE_DIR}
-    --no-first-run
-    --no-default-browser-check
-    --disable-extensions
-    --disable-features=TranslateUI,Translate,AcceptCHFrame,AutoExpandDetailsElement,AvoidUnnecessaryBeforeUnloadCheckSync,CertificateTransparencyComponentUpdater,DestroyProfileOnBrowserClose,DialMediaRouteProvider,ExtensionManifestV2Disabled,GlobalMediaControls,HttpsUpgrades,ImprovedCookieControls,LazyFrameLoading,LensOverlay,MediaRouter,PaintHolding,ThirdPartyStoragePartitioning,DeferRendererTasksAfterInput
-    --force-device-scale-factor=1
-    --disable-geolocation
-    --use-fake-ui-for-media-stream
-    --window-size=${WINDOW_SIZE}
-"
+CHROME_ARGS=(
+    "--remote-debugging-port=${PORT}"
+    "--remote-debugging-address=0.0.0.0"
+    "--user-data-dir=${PROFILE_DIR}"
+    "--no-first-run"
+    "--no-first-run"
+    "--no-default-browser-check"
+    "--disable-extensions"
+    "--disable-features=TranslateUI,Translate,AcceptCHFrame,AutoExpandDetailsElement,AvoidUnnecessaryBeforeUnloadCheckSync,CertificateTransparencyComponentUpdater,DestroyProfileOnBrowserClose,DialMediaRouteProvider,ExtensionManifestV2Disabled,GlobalMediaControls,HttpsUpgrades,ImprovedCookieControls,LazyFrameLoading,LensOverlay,MediaRouter,PaintHolding,ThirdPartyStoragePartitioning,DeferRendererTasksAfterInput"
+    "--force-device-scale-factor=1"
+    "--disable-geolocation"
+    "--use-fake-ui-for-media-stream"
+    "--window-size=${WINDOW_SIZE}"
+)
 
 # Start Chrome
-"$CHROME_CMD" $CHROME_ARGS > /tmp/chrome.log 2>&1 &
+"$CHROME_CMD" "${CHROME_ARGS[@]}" > /tmp/chrome.log 2>&1 &
 
 CHROME_PID=$!
 echo "Chrome PID: $CHROME_PID"
